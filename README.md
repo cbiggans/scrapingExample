b370cd3bfe449fae9db3b61b7bb02f417a352406
Clarence Biggans <clarencebiggans@gmail.com>
2019-06-09 23:47:36 -0700

    Working Example

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # scrapingExample
 Scraping a Webpage using Cheerio Example
+
+install
+-------
+npm install cheerio --save
+npm install axios --save
+npm install request --save
+
+Run
+---
+node index.js
---
+++ index.js
@@ -0,0 +1,43 @@
+let request = require('request')
+let cheerio = require('cheerio')
+const axios = require ('axios')
+
+//
+//Load HTTP module
+const http = require("http");
+const hostname = '127.0.0.1';
+const port = 3001;
+
+//Create HTTP server and listen on port 3000 for requests
+const server = http.createServer((req, res) => {
+
+  //Set the response HTTP header with HTTP status and Content type
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello World\n');
+});
+
+//listen for request on port 3000, and as a callback function have the port listened on logged
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+
+  // axios.get('https://medium.com/data-scraper-tips-tricks/scraping-data-with-javascript-in-3-minutes-8a7cf8275b31')
+  // .then((response) => {
+  //   let $ = cheerio.load(response.data)
+  //   // console.log(Object.getOwnPropertyNames(response))
+  //   console.log('Title: ', $('head > title').text())
+  //   // console.log('Body: ', $('body').text())
+  // })
+  let url = 'https://stackoverflow.com/questions/50592957/get-title-of-external-page-using-javascript'
+  // let url = 'https://medium.com/data-scraper-tips-tricks/scraping-data-with-javascript-in-3-minutes-8a7cf8275b31'
+  request(url, function (error, response, body) { if (error) {
+        console.log(error);
+        return
+    }
+    var $ = cheerio.load(body);
+    var title = $("title").text();
+    console.log(title)
+  });
+
+  // console.log('Title: ', $('body').text())
+});
